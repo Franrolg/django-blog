7f19ca246ff1789cd0d171d7ba01c5024a5d3e2c
franrolg <francisco.roldang0@gmail.com>
2023-06-13 00:32:28 -0400

    feat: creación modelo Post y migración necesaria

--- blog/principal/models.py
+++ blog/principal/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+
+    opciones = (
+        ("borrador", "Borrador"),
+        ("publicado", "Publicado"),
+    )
+
+    titulo = models.CharField(max_length=250)
+    subtitulo = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=250, unique=True)
+    autor = models.ForeignKey(User, on_delete=models.PROTECT, related_name="autor_post")
+    contenido = models.TextField()
+    estado = models.CharField(max_length=10, choices=opciones, default="borrador")
+
+    creado = models.DateTimeField(auto_now_add=True, editable=False)
+    actualizado = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-creado",)
+
+    def __str__(self):
+        return f'{self.titulo} - {self.autor.get_full_name()}'
